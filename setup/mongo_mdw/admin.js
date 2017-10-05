492fea4a3cf616df7c6d237d65c25c998f07950a
Philippe Dessauw <philippe.dessauw@nist.gov>
2017-10-05 10:47:56 -0400

    initial commit

---
+++ setup/mongo_mdw/admin.js
@@ -0,0 +1,15 @@
+db.createUser(
+    {
+        user: "admin",
+
+        pwd: "admin",
+        roles: [
+            {
+                role: "userAdminAnyDatabase",
+                db: "admin"
+            },
+            "backup","restore"
+        ]
+    }
+);
+
---
+++ setup/mongo_mdw/setup_admin_mongo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CONTAINER="integ_mongo_curator"
+HOST=`docker ps -f "name=${CONTAINER}" --format "{{.Names}}"`
+ADMIN_USER="admin"
+ADMIN_PWD="admin"
+
+echo "Creating admin user..."
+docker cp admin.js $HOST:/
+docker exec -it $HOST mongo admin admin.js
